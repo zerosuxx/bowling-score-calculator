6100ef5e89dc64c264e1adb62727dc0b4a95f49b
Mohos Tamás <tomi@mohos.name>
2018-08-02 16:05:30 +0200

    refactor(): iterate over all frames

--- src/bowling-score-calculator.js
+++ src/bowling-score-calculator.js
@@ -1,7 +1,8 @@
 function bowlingScoreCalculator(frames) {
     let score = 0;
-    score += frames[0][0];
-    score += frames[1][0];
+    for(let frameIndex = 0; frameIndex < frames.length; frameIndex++) {
+        score += frames[frameIndex][0];
+    }
     return score;
 }
 

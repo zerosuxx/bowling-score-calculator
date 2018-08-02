f8221be69ac263ebcd6332c32fdd3698c933c387
Mohos Tamás <tomi@mohos.name>
2018-08-02 16:23:19 +0200

    refactor(): frameScore calculate

--- src/bowling-score-calculator.js
+++ src/bowling-score-calculator.js
@@ -1,13 +1,17 @@
 function bowlingScoreCalculator(frames) {
-    let score = 0;
-    for(let frameIndex = 0; frameIndex < frames.length; frameIndex++) {
+    const calculateFrameScore = (frameIndex) => {
         let frameScore = 0;
         frameScore += frames[frameIndex][0];
         frameScore += frames[frameIndex][1];
         if(frameScore === 10) {
             frameScore += frames[frameIndex+1][0];
         }
-        score += frameScore;
+        return frameScore;
+    } 
+
+    let score = 0;
+    for(let frameIndex = 0; frameIndex < frames.length; frameIndex++) {
+        score += calculateFrameScore(frameIndex);
     }
     return score;
 }

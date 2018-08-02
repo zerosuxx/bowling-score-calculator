332ef37790a08274b69c77b6f6bcb3acfc099871
Mohos Tamás <tomi@mohos.name>
2018-08-02 15:47:56 +0200

    feat(bowling-score-calculator): should return one

--- src/bowling-score-calculator.js
+++ src/bowling-score-calculator.js
@@ -1,5 +1,9 @@
-function bowlingScoreCalculator() {
-
+function bowlingScoreCalculator(frames) {
+    let score = 0;
+    frames.forEach(frame => {
+        score += frame.reduce((a, b) => a + b, 0);
+    });
+    return score;
 }
 
 module.exports = bowlingScoreCalculator;
--- src/bowling-score-calculator.spec.js
+++ src/bowling-score-calculator.spec.js
@@ -2,6 +2,14 @@ const bowlingScoreCalculator = require('./bowling-score-calculator');
 
 describe('bowlingScoreCalculator', function() {
   it('should not throw exception', function () {
-    expect(bowlingScoreCalculator([[0, 0], [0, 0], [0, 0], [0, 0], [0, 0], [0, 0], [0, 0], [0, 0], [0, 0], [0, 0]])).to.not.thow;
+    expect(bowlingScoreCalculator([[0, 0], [0, 0], [0, 0], [0, 0], [0, 0], [0, 0], [0, 0], [0, 0], [0, 0], [0, 0]])).to.be.not.throw;
+  });
+
+  it('should return 0', function () {
+    expect(bowlingScoreCalculator([[0, 0], [0, 0], [0, 0], [0, 0], [0, 0], [0, 0], [0, 0], [0, 0], [0, 0], [0, 0]])).to.be.equals(0);
+  });
+
+  it('should return 1', function () {
+    expect(bowlingScoreCalculator([[1, 0], [0, 0], [0, 0], [0, 0], [0, 0], [0, 0], [0, 0], [0, 0], [0, 0], [0, 0]])).to.be.equals(1);
   });
 });

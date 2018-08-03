8188d7c522e6713b2b3205aebf66d3ee7ed5de0d
Mohos Tamás <tomi@mohos.name>
2018-08-03 11:36:28 +0200

    feat(): should handle all strike

--- src/bowling-score-calculator.js
+++ src/bowling-score-calculator.js
@@ -2,33 +2,45 @@ function bowlingScoreCalculator(frames) {
 
     const isSpare = (frame) => frame[0] !== 10 && calculateSimpleFrameScore(frame) === 10;
 
-    const isStrike = (frame) => frame[0] === 10; 
+    const isStrike = (frame) => frame[0] === 10;
 
     const calculateSimpleFrameScore = (frame) => frame[0] + frame[1];
 
     const calculateStrikesScore = (frameIndex) => {
         let score = 0;
-        if(frames.length > frameIndex+1) {
-            score += calculateSimpleFrameScore(frames[frameIndex+1]);
+
+        if (frames.length > frameIndex) {
+            score = calculateSimpleFrameScore(frames[frameIndex]);
         }
         return score;
-    }
+    };
+
+    const isLastFrame = (frameIndex) => frames.length - 1 === frameIndex;
 
     const calculateFrameScore = (frameIndex) => {
         const frame = frames[frameIndex];
+        const nextFrameIndex = frameIndex + 1;
         let frameScore = calculateSimpleFrameScore(frame);
-        if(isSpare(frame)) {
-            frameScore += frames[frameIndex+1][0];
+
+        if (frames.length === nextFrameIndex) {
+            return frameScore;
         }
-        if(isStrike(frame) ) {
-            frameScore += calculateStrikesScore(frameIndex);
-            frameScore += calculateStrikesScore(frameIndex+1);
+
+        if (isSpare(frame)) {
+            frameScore += frames[nextFrameIndex][0];
+        }
+
+        if(isStrike(frame) && isLastFrame(frame) && frame[2]) {
+            frameScore += frame[2];
+        } else if (isStrike(frame)) {
+            frameScore += calculateStrikesScore(nextFrameIndex);
+            frameScore += calculateStrikesScore(nextFrameIndex + 1);
         }
         return frameScore;
     };
 
     let score = 0;
-    for(let frameIndex = 0; frameIndex < frames.length; frameIndex++) {
+    for (let frameIndex = 0; frameIndex < frames.length; frameIndex++) {
         score += calculateFrameScore(frameIndex);
     }
     return score;

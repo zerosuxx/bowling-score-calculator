ec0d07f8fe3c9b3956c82edb5e4c7ea1500be735
Mohos Tamás <tomi@mohos.name>
2018-08-03 13:17:59 +0200

    refactor(): tests

--- src/bowling-score-calculator.spec.js
+++ src/bowling-score-calculator.spec.js
@@ -21,15 +21,15 @@ describe('bowlingScoreCalculator', function () {
         expect(bowlingScoreCalculator([[2, 0], [2, 0], [0, 1], [0, 0], [0, 0], [0, 0], [0, 0], [0, 0], [0, 0], [0, 0]])).to.be.equals(5);
     });
 
-    it('should handle spare return 12', function () {
+    it('should handle spare', function () {
         expect(bowlingScoreCalculator([[2, 8], [1, 0], [0, 0], [0, 0], [0, 0], [0, 0], [0, 0], [0, 0], [0, 0], [0, 0]])).to.be.equals(12);
     });
 
-    it('should handle multiple spare return 33', function () {
+    it('should handle multiple spare', function () {
         expect(bowlingScoreCalculator([[2, 8], [3, 7], [5, 0], [0, 0], [0, 0], [0, 0], [0, 0], [0, 0], [0, 0], [0, 0]])).to.be.equals(33);
     });
 
-    it('should handle strike return 24', function () {
+    it('should handle strike', function () {
         expect(bowlingScoreCalculator([[10, 0], [5, 2], [0, 0], [0, 0], [0, 0], [0, 0], [0, 0], [0, 0], [0, 0], [0, 0]])).to.be.equals(24);
     });
 
@@ -40,4 +40,5 @@ describe('bowlingScoreCalculator', function () {
     it('should handle all strike', function () {
         expect(bowlingScoreCalculator([[10, 0], [10, 0], [10, 0], [10, 0], [10, 0], [10, 0], [10, 0], [10, 0], [10, 0], [10, 10, 10]])).to.be.equals(300);
     });
+
 });
